660820086cfc14706be6166c34e06360201f5c29
MANIKANDAN S <smani2887@gmail.com>
2023-12-13 19:37:54 +0530

    NodeJS-6 Synrous Original

--- syns.js
+++ syns.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-fs.readFile('E:/MANI/Project/Zen_class/NodeJS/Practice/test.txt','utf8',(err, data) => {
-    if(err){
-        console.error(err);
-    }
-    console.log(data.split('\n')); // line by line arrange
-})
+try{
+    const data = fs.readFileSync('E:/MANI/Project/Zen_class/NodeJS/Practice/test.txt','utf8');
+    console.log(data)
+}catch(err){
+    console.log(err)
+}
